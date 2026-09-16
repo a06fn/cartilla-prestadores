334d7e35cfb7ddbbe9f6f22b5a8f7fd388b26de8
bqchi <facundonalmada@gmail.com>
2026-09-16 12:46:32 -0300

    Modificacion tabla seccionales.

---
+++ src/data/seccionales.js
@@ -0,0 +1,389 @@
+export const seccionales = [
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - CABA",
+    "PROVINCIA": "Ciudad Autonoma de Buenos Aires",
+    "PARTIDO": "Comuna 5",
+    "LOCALIDAD": "Almagro",
+    "SECCIONAL": "CAPITAL",
+    "DIRECCION": "Quintino Bocayuva 50",
+    "TELEFONO": "0800-288-8872"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - San Isidro",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "San Isidro",
+    "LOCALIDAD": "San Isidro",
+    "SECCIONAL": "GBA NORTE",
+    "DIRECCION": "Fleming 970",
+    "TELEFONO": "TEL 5555-3461;WSP 11-3321-8669"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Quilmes",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "Quilmes",
+    "LOCALIDAD": "Quilmes",
+    "SECCIONAL": "GBA SUR",
+    "DIRECCION": "Vicente López 87",
+    "TELEFONO": "4252-3856"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - La Plata",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "La Plata",
+    "LOCALIDAD": "La Plata",
+    "SECCIONAL": "LA PLATA",
+    "DIRECCION": "Calle 39 789",
+    "TELEFONO": "0221 4222600;0221 4252618"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Mercedes",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "Mercedes",
+    "LOCALIDAD": "Mercedes",
+    "SECCIONAL": "BUENOS AIRES ZONA 1",
+    "DIRECCION": "Calle 23 número 531",
+    "TELEFONO": "02324 424101;02324 430514;02324 430514"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Las Flores",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "Las Flores",
+    "LOCALIDAD": "Las Flores",
+    "SECCIONAL": "BUENOS AIRES ZONA 2",
+    "DIRECCION": "Belgrano 734",
+    "TELEFONO": "02244 453121;0224 4 443121"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Junin",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "Junin",
+    "LOCALIDAD": "Junin",
+    "SECCIONAL": "BUENOS AIRES ZONA 3",
+    "DIRECCION": "Ameghino 35",
+    "TELEFONO": "02362 421721"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Mar del Plata",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "General Pueyrredon",
+    "LOCALIDAD": "Mar Del Plata",
+    "SECCIONAL": "MAR DEL PLATA",
+    "DIRECCION": "Olazabal 1357",
+    "TELEFONO": "0223 4723216;0223 4751186;0223 4750905"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Bahía Blanca",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "Bahia Blanca",
+    "LOCALIDAD": "Bahia Blanca",
+    "SECCIONAL": "BAHIA BLANCA",
+    "DIRECCION": "Sarmiento 799",
+    "TELEFONO": "0291 4532772;549 291 4127875"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Trenque Lauquen",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "Trenque Lauquen",
+    "LOCALIDAD": "Trenque Lauquen",
+    "SECCIONAL": "TRENQUE LAUQUEN",
+    "DIRECCION": "25 de Mayo 244",
+    "TELEFONO": "02392-425333;02392-415333"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - General Pico",
+    "PROVINCIA": "La Pampa",
+    "PARTIDO": "Maraco",
+    "LOCALIDAD": "General Pico",
+    "SECCIONAL": "LA PAMPA",
+    "DIRECCION": "Calle 5 Nº 142 e/4 y 6",
+    "TELEFONO": "02302-4304 01"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Santa Rosa",
+    "PROVINCIA": "La Pampa",
+    "PARTIDO": "La Pampa Capital",
+    "LOCALIDAD": "Santa Rosa",
+    "SECCIONAL": "LA PLATA",
+    "DIRECCION": "Garibaldi 104",
+    "TELEFONO": "02954-434123"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Río Negro",
+    "PROVINCIA": "Rio Negro",
+    "PARTIDO": "General Roca",
+    "LOCALIDAD": "General Roca",
+    "SECCIONAL": "CORDOBA",
+    "DIRECCION": "25 de Mayo 562",
+    "TELEFONO": "0298-4432812"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Neuquén",
+    "PROVINCIA": "Neuquen",
+    "PARTIDO": "Confluencia",
+    "LOCALIDAD": "Neuquen",
+    "SECCIONAL": "NEUQUEN",
+    "DIRECCION": "Pehuén Pasaje del Sol 890",
+    "TELEFONO": "0299-4 428530"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Esquel",
+    "PROVINCIA": "Chubut",
+    "PARTIDO": "Futaleufu",
+    "LOCALIDAD": "Esquel",
+    "SECCIONAL": "CHUBUT",
+    "DIRECCION": "Perito Moreno 766 Dto. 4",
+    "TELEFONO": "02945-455871"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Pto. Madryn",
+    "PROVINCIA": "Chubut",
+    "PARTIDO": "Biedma",
+    "LOCALIDAD": "Puerto Madryn",
+    "SECCIONAL": "CHUBUT",
+    "DIRECCION": "Mitre 150",
+    "TELEFONO": "0280-4474855"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Trelew",
+    "PROVINCIA": "Chubut",
+    "PARTIDO": "Rawson",
+    "LOCALIDAD": "Trelew",
+    "SECCIONAL": "CHUBUT",
+    "DIRECCION": "Joseph Jones 184",
+    "TELEFONO": "0280-4422438"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Comodoro Rivadavia",
+    "PROVINCIA": "Chubut",
+    "PARTIDO": "Escalante",
+    "LOCALIDAD": "Comodoro Rivadavia",
+    "SECCIONAL": "CHUBUT",
+    "DIRECCION": "Ramon Huenchucheo 253 P Palazzo",
+    "TELEFONO": "0297-4063426; 297 405-8722"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Santa Cruz",
+    "PROVINCIA": "Santa Cruz",
+    "PARTIDO": "Guer Aike",
+    "LOCALIDAD": "Rio Gallegos",
+    "SECCIONAL": "SANTA CRUZ",
+    "DIRECCION": "Pje. Zucarino 541",
+    "TELEFONO": "02966-423282;02966-439846"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Río Grande",
+    "PROVINCIA": "Tierra del Fuego",
+    "PARTIDO": "Rio Grande",
+    "LOCALIDAD": "Rio Grande",
+    "SECCIONAL": "SAN LUIS",
+    "DIRECCION": "Piedrabuena 976",
+    "TELEFONO": "02964-426761"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Ushuaia",
+    "PROVINCIA": "Tierra del Fuego",
+    "PARTIDO": "Ushuaia",
+    "LOCALIDAD": "Ushuaia",
+    "SECCIONAL": "TIERRA DEL FUEGO",
+    "DIRECCION": "Malvinas Argentinas 20",
+    "TELEFONO": "02901-42 3280"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Rosario",
+    "PROVINCIA": "Santa Fe",
+    "PARTIDO": "Rosario",
+    "LOCALIDAD": "Rosario",
+    "SECCIONAL": "ROSARIO",
+    "DIRECCION": "Av. Francia 1231",
+    "TELEFONO": "0341-4373272;0341-4301158;0341-4371331"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Santa Fe",
+    "PROVINCIA": "Santa Fe",
+    "PARTIDO": "Santa Fe Capital",
+    "LOCALIDAD": "Santa Fe",
+    "SECCIONAL": "SANTA FE",
+    "DIRECCION": "Mitre 3340",
+    "TELEFONO": "0342-4553149;0342-4566147"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Entre Ríos",
+    "PROVINCIA": "Entre Rios",
+    "PARTIDO": "Parana",
+    "LOCALIDAD": "Parana",
+    "SECCIONAL": "ENTRE RIOS",
+    "DIRECCION": "Av. Francisco Ramírez 2834",
+    "TELEFONO": "0343-4311671;0343-4233460"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Córdoba",
+    "PROVINCIA": "Cordoba",
+    "PARTIDO": "Cordoba Capital",
+    "LOCALIDAD": "Cordoba",
+    "SECCIONAL": "CORDOBA",
+    "DIRECCION": "Laprida 131 Barrio Nva. Córdoba",
+    "TELEFONO": "0351-4223338;0351-4220163"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Río Cuarto",
+    "PROVINCIA": "Cordoba",
+    "PARTIDO": "Rio Cuarto",
+    "LOCALIDAD": "Rio Cuarto",
+    "SECCIONAL": "CORDOBA",
+    "DIRECCION": "Pcia. De La Rioja 986",
+    "TELEFONO": "0358-4642581"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - San Luis",
+    "PROVINCIA": "San Luis",
+    "PARTIDO": "Juan Martin de Pueyrredon",
+    "LOCALIDAD": "San Luis",
+    "SECCIONAL": "SAN LUIS",
+    "DIRECCION": "Maipú 666",
+    "TELEFONO": "0266-4427911"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Mendoza",
+    "PROVINCIA": "Mendoza",
+    "PARTIDO": "Mendoza Capital",
+    "LOCALIDAD": "2a Seccion",
+    "SECCIONAL": "MENDOZA",
+    "DIRECCION": "Gutierrez 543",
+    "TELEFONO": "0261-4233155;0261-4258418"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - San Juan",
+    "PROVINCIA": "San Juan",
+    "PARTIDO": "San Juan Capital",
+    "LOCALIDAD": "San Juan",
+    "SECCIONAL": "SAN JUAN",
+    "DIRECCION": "Caseros Sur 672",
+    "TELEFONO": "0264-4201310"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - La Rioja",
+    "PROVINCIA": "La Rioja",
+    "PARTIDO": "La Rioja Capital",
+    "LOCALIDAD": "La Rioja Capital",
+    "SECCIONAL": "LA RIOJA",
+    "DIRECCION": "Ramírez de Velazco 1016",
+    "TELEFONO": "0380-4425014;0380-4435482"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Catamarca",
+    "PROVINCIA": "Catamarca",
+    "PARTIDO": "San Fernando del Valle de Catamarca",
+    "LOCALIDAD": "San Fernando del Valle de Catamarca",
+    "SECCIONAL": "CATAMARCA",
+    "DIRECCION": "Fidel Mardoqueo Castro 543 (Sarmiento Norte)",
+    "TELEFONO": "0383-4557352;0383-4431232"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Santiago del Estero",
+    "PROVINCIA": "Santiago del Estero",
+    "PARTIDO": "Santiago Capital",
+    "LOCALIDAD": "Santiago Del Estero",
+    "SECCIONAL": "SANTIAGO DEL ESTERO",
+    "DIRECCION": "Entre Ríos 416",
+    "TELEFONO": "0385-4215378;0385-4256516"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Corrientes",
+    "PROVINCIA": "Corrientes",
+    "PARTIDO": "Corrientes Capital",
+    "LOCALIDAD": "Corrientes",
+    "SECCIONAL": "CORRIENTES",
+    "DIRECCION": "Necochea 3058",
+    "TELEFONO": "0379-4429113"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Chaco",
+    "PROVINCIA": "Chaco",
+    "PARTIDO": "San Fernando Chaco",
+    "LOCALIDAD": "Resistencia",
+    "SECCIONAL": "CHACO",
+    "DIRECCION": "Av. Rivadavia 1230",
+    "TELEFONO": "0362-4438310"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Misiones",
+    "PROVINCIA": "Misiones",
+    "PARTIDO": "Misiones Capital",
+    "LOCALIDAD": "Posadas",
+    "SECCIONAL": "MISIONES",
+    "DIRECCION": "Rebollo 2265 33",
+    "TELEFONO": "0376-4434186;0376-4433831"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Tucumán",
+    "PROVINCIA": "Tucuman",
+    "PARTIDO": "Tucuman Capital",
+    "LOCALIDAD": "San Miguel de Tucuman",
+    "SECCIONAL": "TUCUMAN",
+    "DIRECCION": "Santa Fe 1270",
+    "TELEFONO": "0381-4334166;0381-4334168"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Formosa",
+    "PROVINCIA": "Formosa",
+    "PARTIDO": "Formosa",
+    "LOCALIDAD": "Formosa",
+    "SECCIONAL": "FORMOSA",
+    "DIRECCION": "Carlos Castañeda 75",
+    "TELEFONO": "0370-4430837"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Salta",
+    "PROVINCIA": "Salta",
+    "PARTIDO": "Salta Capital",
+    "LOCALIDAD": "Salta",
+    "SECCIONAL": "SALTA",
+    "DIRECCION": "Alberdi 524",
+    "TELEFONO": "0387-4219544;0387-154142389;0387-154489982"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Jujuy",
+    "PROVINCIA": "Jujuy",
+    "PARTIDO": "Dr Manuel Belgrano",
+    "LOCALIDAD": "San Salvador de Jujuy",
+    "SECCIONAL": "JUJUY",
+    "DIRECCION": "Curupayti 375",
+    "TELEFONO": "0388-4228008;0388-4224667"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - San Justo",
+    "PROVINCIA": "Buenos Aires",
+    "PARTIDO": "La Matanza",
+    "LOCALIDAD": "San Justo",
+    "SECCIONAL": "GBA OESTE",
+    "DIRECCION": "Presidente Juan Domingo Peron 3129",
+    "TELEFONO": "4441-0092;4441-0098"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Caleta Olivia",
+    "PROVINCIA": "Santa Cruz",
+    "PARTIDO": "Deseado",
+    "LOCALIDAD": "Caleta Olivia",
+    "SECCIONAL": "SANTA CRUZ",
+    "DIRECCION": "Independencia 1171 barrio Unión",
+    "TELEFONO": "WSP 2976 07-7276"
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Concordia",
+    "PROVINCIA": "Entre Rios",
+    "PARTIDO": "Concordia",
+    "LOCALIDAD": "Concordia",
+    "SECCIONAL": "ENTRE RIOS",
+    "DIRECCION": "CONCEJAL VEIGA 638",
+    "TELEFONO": "3454018973 "
+  },
+  {
+    "NOMBRE SECCIONAL": "TVSALUD - Gualeguaychú",
+    "PROVINCIA": "Entre Rios",
+    "PARTIDO": "Gualeguaychu",
+    "LOCALIDAD": "Gualeguaychu",
+    "SECCIONAL": "ENTRE RIOS",
+    "DIRECCION": "ANDRADE 1058",
+    "TELEFONO": ""
+  }
+]
