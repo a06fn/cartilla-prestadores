2dd5fd2293a1ff19fe647599ebf7591900b91fd3
bqchi <facundonalmada@gmail.com>
2026-09-16 12:22:28 -0300

    Cambios para separar las tablas del archivo App.js

---
+++ src/planes.js
@@ -0,0 +1,5 @@
+export const planes = [
+    { nombre: 'UNIVERSAL', descripcion: '100, 200, OSPTV' },
+    { nombre: 'INTERMEDIO', descripcion: '300, SAT, JSAT, JSATM'},
+    { nombre: 'SUPERADOR', descripcion: '400, SAT1001' }
+];
