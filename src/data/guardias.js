5b353478a5ccbc7af74fedd5bdff0c32bf4d2600
bqchi <facundonalmada@gmail.com>
2026-09-17 02:08:04 -0300

    Se agrega guardias.js, para separar el servicio de guardia del servicio prestacional. Se quitan las etiquetas de gUardia

---
+++ src/data/guardias.js
@@ -0,0 +1,9 @@
+export const guardias = ['GUARDIA CARDIOLOGIA', 
+                        'GUARDIA GINECOLOGIA', 
+                        'GUARDIA GENERAL', 
+                        'GUARDIA PEDIATRICA', 
+                        'GUARDIA OBSTETRICIA', 
+                        'GUARDIA OFTALMOLOGIA', 
+                        'GUARDIA PSIQUIATRICA', 
+                        'GUARDIA ODONTOLOGICA', 
+                        'GUARDIA TRAUMATOLOGICA'];
